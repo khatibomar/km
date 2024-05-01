9c991566ec3988a47ac8b90dcc0005a82b6ac655
khatibomar <elkhatibomar@outlook.com>
2024-05-01 15:03:59 +0300

    chore: add inital log setup

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func init() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+}
+
+func main() {
+	log.Info().Msg("Initial")
+}
